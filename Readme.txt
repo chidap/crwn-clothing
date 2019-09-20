f3bfc711113cb39024e6237121c1be66a7a297a3
Chida <chida_p@yahoo.com>
2019-09-20 09:44:21 +0530

    update our menu-items to navigate withRouter HOC

--- Readme.txt
+++ Readme.txt
@@ -3,4 +3,15 @@ npx create-react-app crwn-clothing
 1. Create homepage.component.jsx
 2. Add node-sass package for styling
 	yarn add node-sass
-3. Create homepage.style.scss for styling homepage
+3. Create homepage.style.scss for styling homepage         
+
+
+Implementing Router - React Router Dom  
+
+1. yarn add react-router-dom
+2. Import BrowserRouter in index.js and the following in index.js
+	ReactDOM.render(
+    		<BrowserRouter>
+        		<App />
+    		</BrowserRouter>, document.getElementById('root'));
+                                                                                                                                                                                                                                          
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import "./menu-item.styles.scss"
-const MenuItem = ({title, imageUrl, size }) => (
-    <div className = {`${size} menu-item`}>
+import { withRouter } from "react-router-dom";
+import "./menu-item.styles.scss";
+
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match }) => (
+    <div className = {`${size} menu-item`} onClick = {() => history.push(`${match.url}${linkUrl}`)}>
         <div className = "background-image"
             style = {{
                 backgroundImage: `url(${imageUrl})`
@@ -14,4 +16,10 @@ const MenuItem = ({title, imageUrl, size }) => (
     </div>
 )
 
-export default MenuItem;
+/*
+ WithRouter is a higher order component, which essentially a function that takes a component 
+ as an argument and returns a new modified component.Here powering up MenuItem component to have access 
+ related to Router (location, match and history props).
+
+*/
+export default withRouter(MenuItem);
